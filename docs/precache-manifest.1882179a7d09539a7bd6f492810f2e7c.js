369b4072bcecb9b0b33551c457922f92b13f74fe
bigfoe313 <75146345+bigfoe313@users.noreply.github.com>
2022-01-12 20:10:48 -0500

    Update precache-manifest.1882179a7d09539a7bd6f492810f2e7c.js

--- docs/precache-manifest.1882179a7d09539a7bd6f492810f2e7c.js
+++ docs/precache-manifest.1882179a7d09539a7bd6f492810f2e7c.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
     "revision": "229c360febb4351a89df",
-    "url": "/static/js/runtime~main.229c360f.js"
+    "url": "/bigfoe313.gighub.io/docs/static/js/runtime~main.229c360f.js"
   },
   {
     "revision": "c950c0c47951ee277fd4",
-    "url": "/static/js/main.c950c0c4.chunk.js"
+    "url": "/bigfoe313.gighub.io/docs/static/js/main.c950c0c4.chunk.js"
   },
   {
     "revision": "a2e64a37186cf99dd37a",
-    "url": "/static/js/1.a2e64a37.chunk.js"
+    "url": "/bigfoe313.gighub.io/docs/static/js/1.a2e64a37.chunk.js"
   },
   {
     "revision": "c950c0c47951ee277fd4",
-    "url": "/static/css/main.28ac1556.chunk.css"
+    "url": "/bigfoe313.gighub.io/docs/static/css/main.28ac1556.chunk.css"
   },
   {
     "revision": "a2e64a37186cf99dd37a",
-    "url": "/static/css/1.4e638e1d.chunk.css"
+    "url": "/bigfoe313.gighub.io/docs/static/css/1.4e638e1d.chunk.css"
   },
   {
     "revision": "3ccddc3122574cc938702ce6c18bd26b",
-    "url": "/index.html"
+    "url": "/bigfoe313.gighub.io/docs/index.html"
   }
 ];
