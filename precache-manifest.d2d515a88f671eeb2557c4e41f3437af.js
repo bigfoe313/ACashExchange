c41b75888b6c01aec3ca5f26f5164120bdf7e5b1
bigfoe313 <aforbers@gmail.com>
2022-03-01 21:16:05 -0500

    Updates

---
+++ precache-manifest.d2d515a88f671eeb2557c4e41f3437af.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dc6e6ec0aaa07b1df57f",
+    "url": "/ACashExchange/static/css/main.af96ef96.chunk.css"
+  },
+  {
+    "revision": "dc6e6ec0aaa07b1df57f",
+    "url": "/ACashExchange/static/js/main.dc6e6ec0.chunk.js"
+  },
+  {
+    "revision": "7c67a705dbcd2faf3d60",
+    "url": "/ACashExchange/static/css/1.6afa3063.chunk.css"
+  },
+  {
+    "revision": "7c67a705dbcd2faf3d60",
+    "url": "/ACashExchange/static/js/1.7c67a705.chunk.js"
+  },
+  {
+    "revision": "fa15b7e9cef41c3fda6c",
+    "url": "/ACashExchange/static/js/runtime~main.fa15b7e9.js"
+  },
+  {
+    "revision": "7aa5d7e4c5a63f98aed272727bee01ee",
+    "url": "/ACashExchange/index.html"
+  }
+];
